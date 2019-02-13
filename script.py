06b0fa903a41e8598bae28cc835b448717d72590
Arunava <learningdroidarunava@gmail.com>
2019-02-13 13:13:41 +0530

    Update script.py

--- script.py
+++ script.py
@@ -29,7 +29,7 @@ trainloader = DataLoader(train_data, batch_size=batch_size, shuffle=True)
 def init_weight(m):
   classname = m.__class__.__name__
   if classname.find('Conv') != -1:
-    m.weight.data.normal_(0.0, 0.2)
+    m.weight.data.normal_(0.0, 0.02)
   elif classname.find('BatchNorm') != -1:
     m.weight.data.normal_(1.0, 0.02)
     m.bias.data.fill_(0)
